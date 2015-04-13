deb9b64915c067fa5e361fb4744d8a7bb37612df
angus1105 <angus1105@gmail.com>
2015-04-13 14:26:23 +0800

    Initial Commit

---
+++ iDuoYu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iDuoYu
+//
+//  Created by ChenAngus on 15/4/13.
+//  Copyright (c) 2015年 iduoyu.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iDuoYu/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iDuoYu
+//
+//  Created by ChenAngus on 15/4/13.
+//  Copyright (c) 2015年 iduoyu.com. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
